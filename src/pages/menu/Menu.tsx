b9c1a230f79179ed2d12c44dbe6fbe253c729042
charmingvenca <klimes.vasik2006@gmail.com>
2024-07-27 16:34:38 -0400

    added menu items with styling

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -1,16 +1,30 @@
 import { Base } from "../../shared/BasePage";
-import {Navbar} from "../../shared/Navbar";
+import { Navbar } from "../../shared/Navbar";
+import { Link } from "react-router-dom";
+
+const userInitials = "VK"
 
 export const Menu = () => {
   return (
     <Base>
-      <ul className="menu bg-base-200 rounded-box w-56">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-        <li><a>Item 3</a></li>
-      </ul>
+      {/*Avatar*/}
+      <div className="avatar placeholder flex justify-end w-full">
+        <div className="bg-gray-800 text-white w-16 rounded-full">
+          <span className="text-3xl">{userInitials}</span>
+        </div>
+      </div>
 
+      {/*Menu items*/}
+      <div className="flex flex-col items-center justify-start min-h-screen">
+        <ul className="menu bg-white rounded-box w-full shadow-lg">
+          <li className="menu-item w-full border-b border-gray-300"><Link to="/settings">Settings</Link></li>
+          <li className="menu-item w-full border-b border-gray-300"><Link to="/statistics">Statistics</Link></li>
+          <li className="menu-item w-full border-b border-gray-300"><Link to="/temp1">Temporary Item 1</Link></li>
+          <li className="menu-item w-full border-b border-gray-300"><Link to="/temp2">Temporary Item 2</Link></li>
+          <li className="menu-item w-full"><Link to="/temp3">Temporary Item 3</Link></li>
+        </ul>
+      </div>
       <Navbar/>
     </Base>
-  )
+  );
 }
