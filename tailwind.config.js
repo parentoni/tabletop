ad315ddbbb69ddd90174b2a9b6b05fbe553c5190
arthuroi <parentoni.arthur@gmail.com>
2024-07-27 15:19:58 -0400

    feat: daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   
   content: ["./src/**/*.{tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
- plugins: [],
+  daisyui: {
+    themes: [{
+      light: {
+        ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+        "primary": "FFA1F5",
+        "hover": "rgb(0, 0, 0, 0.05)"
+      }
+    }]
+  },
+ plugins: [require("daisyui")],
 }
